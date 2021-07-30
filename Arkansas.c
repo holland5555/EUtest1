8d491652e77963c9178a2cba3bf7fb8db73472a4
holland044 <holland044@hotmail.com>
2021-07-30 09:51:19 -0700

    Add files via upload

---
+++ Arkansas.c
@@ -0,0 +1,15 @@
+Actually it's Nebraska
+
+ Testing SSN/Special Words/Email/URLs (RegEx - PII)
+
+ Testing Passwords and Tokens
+
+ Testing Sensitive Words
+
+
+
+
+
+
+
+  
---
+++ NewYork.c
@@ -0,0 +1,12 @@
+ github_client-id : 'c1332c94c47799b03acc'
+   
+   Hello
+   
+ github_client-id : 'c1332c94c46999b03acc'
+  
+  password="dlfjaopefinweqpofnqewfoqnffpoqenfpqowefnq"
+  
+  maurice@blubracket.com
+  444-55-6667;
+  https://hello.hellothere.com
+
